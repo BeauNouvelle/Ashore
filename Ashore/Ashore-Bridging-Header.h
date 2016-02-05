4e91ee166a40793b61a57025100fe86bc77d01a6
Beau Nouvelle <casuist@icloud.com>
2016-02-05 22:39:10 +1100

    V1.0 app store. Added Heap

---
+++ Ashore/Ashore-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Ashore-Bridging-Header.h
+//  Ashore
+//
+//  Created by Beau Young on 5/02/2016.
+//  Copyright © 2016 Beau Nouvelle. All rights reserved.
+//
+
+#ifndef Ashore_Bridging_Header_h
+#define Ashore_Bridging_Header_h
+
+#import "Heap.h"
+
+#endif /* Ashore_Bridging_Header_h */
